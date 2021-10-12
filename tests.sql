97540da1947e2e5427bb808ee8740510af5fef71
Natalie Gordon <e0425633@u.nus.edu>
2021-10-12 22:24:55 +0800

    Added tests.sql
    populate with tests here; test for failures and exceptions too

---
+++ tests.sql
@@ -0,0 +1,9 @@
+/* Function (1) add_employee (Fabian) */
+
+/* Set 1: Verify that the function works in the normal case (Passing) */
+CALL add_employee ('Grace', '98765432', 'Junior', 'Accounting')
+CALL add_employee ('Natalie', '12345678', 'Manager', 'HR')
+CALL add_employee ('Gareth', '99998888', 'Senior', 'Accounting')
+CALL add_employee ('Kevin', '88889999', 'Junior', 'Marketing')
+
+
